1da073004cd7c839a2665717bb49486748ff765e
Ritanshu tiwari <ritanshutiwari100@gmail.com>
2026-08-27 23:36:11 +0530

    Update JavaScript projects

--- JAVA SCRIPT/index39.js
+++ JAVA SCRIPT/index39.js
@@ -1,12 +1,30 @@
-//   Visualizing the Call Stack--.
+// var user={
+//     fName:"John",
+//     lName:"Doe",
+//     age:30,
+//     getFullName: function(){ // Define a method to get the full name
+//         return user.fName + " " + user.lName; // Concatenate first name and last name
+//     },
+//     getAgeYear: function(){ // Define a method to get the age in years
+//         return new Date().getFullYear() - user.age; // Calculate the year of birth based on the current year and age
+// }
+// }
 
-function one(){
-    return 1;
+
+//  Factory function to create user objects
+function createUser(firstName, lastName, age){
+    return {
+        fName: firstName,
+        lName: lastName,
+        age: age,
+        getFullName: function(){
+            return this.fName + " " + this.lName;
+        },
+        getAgeYear: function(){
+            return new Date().getFullYear() - this.age;
+        }
+    }
+    return user; // Return the created user object
 }
-function two(){
-    return one() + one();
-}
-function three(){
-    let ans = two() + one();
-    console.log(ans);
-    three()};
+const user1=createUser("John", "Doe", 30)
+const user2=createUser("Jane", "Smith", 25)
