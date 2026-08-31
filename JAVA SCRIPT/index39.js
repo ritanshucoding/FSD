2dcd5cc1f9a3078493874294203d2e267772a0ea
Ritanshu tiwari <ritanshutiwari100@gmail.com>
2026-08-31 23:11:40 +0530

    Update JavaScript projects

--- JAVA SCRIPT/index39.js
+++ JAVA SCRIPT/index39.js
@@ -12,19 +12,34 @@
 
 
 //  Factory function to create user objects
-function createUser(firstName, lastName, age){
-    return {
-        fName: firstName,
-        lName: lastName,
-        age: age,
-        getFullName: function(){
-            return this.fName + " " + this.lName;
-        },
-        getAgeYear: function(){
-            return new Date().getFullYear() - this.age;
-        }
-    }
-    return user; // Return the created user object
+
+// function createUser(firstName, lastName, age){
+//     return {
+//         fName: firstName,
+//         lName: lastName,
+//         age: age,
+//         getFullName: function(){
+//             return this.fName + " " + this.lName;
+//         },
+//         getAgeYear: function(){
+//             return new Date().getFullYear() - this.age;
+//         }
+//     }
+//     return user; // Return the created user object
+// }
+// const user1=createUser("John", "Doe", 30)
+// const user2=createUser("Jane", "Smith", 25)
+
+// New Keyword
+// function greet(){
+//     this.a="Hello";
+//     this.b="World";
+// }
+
+function createUser(firstName, lastName, age){ // Constructor function to create user objects
+    this.fName=firstName;
+    this.lName=lastName;
+    this.age=age;
 }
-const user1=createUser("John", "Doe", 30)
-const user2=createUser("Jane", "Smith", 25)
+const user1=new createUser("John", "Doe", 30); // Create a new user object using the constructor function
+const user2=new createUser("Jane", "Smith", 25); // Create another user object using the constructor function
