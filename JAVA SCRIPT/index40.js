873ef3c53256a524b8f4b0e9a320b6cca30aa071
Ritanshu tiwari <ritanshutiwari100@gmail.com>
2026-09-02 19:23:45 +0530

    Update JavaScript projects

--- JAVA SCRIPT/index40.js
+++ JAVA SCRIPT/index40.js
@@ -1,60 +1,26 @@
-// h1 = document.querySelector("h1");
-// setTimeout(() => {
-//     h1.style.color = "red";
-// }, 1000);
-// setTimeout(() => {
-//     h1.style.color = "blue";
-// }, 2000);
-// setTimeout(() => {
-//     h1.style.color = "green";
-// }, 3000);
-// setTimeout(() => {
-//     h1.style.color = "yellow";
-// }, 4000);
+// Timers
 
-h1 = document.querySelector("h1");
+setTimeout(function(){
+    console.log("Hello")
+},3000); // Set a timeout to log "Hello" after 3000 milliseconds (3 seconds)
 
-// function changecolor(color , delay ,nextColorChange){
-//     setTimeout(() => {
-//         h1.style.color = color;
-//         if(nextColorChange)nextColorChange();
-//     }, delay);
-// }
-// changecolor("red",1000,() => {
-//     changecolor("blue",2000,() => {
-//         changecolor("green",3000,() => {
-//             changecolor("yellow",4000);
-//         });
-//     });
-// });
+setInterval(function(){
+    console.log("Hello")
+},3000); // Set an interval to log "Hello" every 3000 milliseconds (3 seconds)
 
-//  CALLBACK NESTING -> callback hell
-
-function changecolor(color, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            h1.style.color = color;
-            resolve("color changed!");
-        }, delay);
-    });
-}
-changecolor("red", 1000)
-    .then(() => {
-        console.log("color changed to blue");
-        return changecolor("blue", 2000);
-    })
-    .then(() => {
-        console.log("color changed to green");
-        return changecolor("green", 3000);
-    })
-    .then(() => {
-        console.log("color changed to yellow");
-        return changecolor("yellow", 4000);
-    })
-    .then(() => {
-        console.log("color changed to orange");
-        return changecolor("orange", 5000);
-    })
-    .catch(() => {
-        console.log("error");
-    });
+//  Nested Callbacks (Callback Hell)
+setTimeout(function(){
+    console.log("Hello")
+    setTimeout(function(){
+        console.log("Hello")
+        setTimeout(function(){
+            console.log("Hello")
+            setTimeout(function(){
+                console.log("Hello")
+                setTimeout(function(){
+                    console.log("Hello")
+                },1000)
+            },2000)
+        },3000)
+    },4000)
+},5000) // Set a series of nested timeouts to log "Hello" every 3 seconds, creating a callback hell scenario
